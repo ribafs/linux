0a31e29021b5b7a19aa3cab3be10de5bd5806d92
Ribamar FS <ribafs@gmail.com>
2021-03-06 16:32:44 -0300

    up

--- Scripts/wget.sh
+++ Scripts/wget.sh
@@ -1,3 +1,5 @@
+# Exportar um site dinâmico completo para html estático. Sites criados em Joomla, Wordpress, aplicativos com frameworks, etc. O site abaixo foi criado com Joomla e agora está no Github de gratis. O original não existe mais, que era ribafs.org:
+# https://ribafs.github.io/portal/
 wget \
      --recursive \
      --no-clobber \
@@ -6,6 +8,6 @@ wget \
      --convert-links \
      --restrict-file-names=windows \
      --no-parent \
-         $1
-# Exemplo:
+     $1
+# Exemplo
 # sh wget https://ribafs.org
